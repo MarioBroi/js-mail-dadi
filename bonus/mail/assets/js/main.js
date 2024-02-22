04c3b56f8fc0c063919f37abc923611261d4326c
MarioBroi <inoix16@gmail.com>
2024-02-22 15:48:25 +0000

    edit email exercise - edit js to print the login result - add css style

--- bonus/mail/assets/js/main.js
+++ bonus/mail/assets/js/main.js
@@ -6,16 +6,15 @@ function myFunction() {
     let email = document.getElementById('myInput').value;
     const usersList = ['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com'];
     let login = "You are not registered";
-    //console.log(email); // Typed text
 
     for (let index = 0; index < usersList.length; index++) {
 
         if (email === usersList[index]) {
-            //console.log("You can join");
             login = "You can join"
+
         }
     }
 
     console.log(login); //login message
-
+    document.getElementById("output").innerHTML = `<p>${login}</p>`;
 }
