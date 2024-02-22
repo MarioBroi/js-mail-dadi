ee2cccdac91a5d52a1d3971ba2825aba922cc815
MarioBroi <inoix16@gmail.com>
2024-02-22 13:38:20 +0000

    add Dadi part of exercise - add variable definition - add if to check wich number is higher

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 //console.log('Test');
 
+//MAIL
+
 let email = prompt('Type your E-Mail');
 const usersList = ['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com'];
 let login = "You are not registered";
@@ -14,3 +16,18 @@ for (let index = 0; index < usersList.length; index++) {
 }
 
 console.log(login); //login message
+
+// DADI
+
+const min = 1;
+const max = 6;
+let userRandom = Math.random() * (max - min) + min;
+console.log(userRandom); //number for user
+let npcRandom = Math.random() * (max - min) + min;
+console.log(npcRandom); //number for npc
+
+if (userRandom > npcRandom) {
+    console.log('you win');
+} else {
+    console.log('you lose');
+}
