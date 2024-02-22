631dbc96339695ef16a7d2ad8c30c0b728f9e3ac
MarioBroi <inoix16@gmail.com>
2024-02-22 13:28:50 +0000

    add Mail part of the exercise - add variable definition - add for loop - add if to check vaiables - add console.log to print the result

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log('Test');
+//console.log('Test');
+
+let email = prompt('Type your E-Mail');
+const usersList = ['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com'];
+let login = "You are not registered";
+//console.log(email); // Typed text
+
+for (let index = 0; index < usersList.length; index++) {
+
+    if (email === usersList[index]) {
+        //console.log("You can join");
+        login = "You can join"
+    }
+}
+
+console.log(login); //login message
